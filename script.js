668642293bff17d5f5003a4a53a8bb8aa222d78f
Antoni <antoni-devlin@users.noreply.github.com>
2024-02-19 10:05:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-javascript:!function(){let b=window.location.href,c=window.location.pathname;if(b.includes("gov.uk"))location="https://my2.siteimprove.com/QualityAssurance/1112165/Overview/Search?SearchIn=Url&Query="+c.replace(/\//g,"%2F");else{let a=prompt("Enter a url to search for in siteimprove:\r\ne.g. %27/contact-dfe%27","").replace(/\//g,"%2F");a&&(location="https://my2.siteimprove.com/QualityAssurance/1112165/Overview/Search?SearchIn=Url&Query="+escape(a))}}()
+function () {
+    let b = window.location.href,
+        c = window.location.pathname;
+    if (b.includes("gov.uk")) location = "https://my2.siteimprove.com/QualityAssurance/1112165/Overview/Search?SearchIn=Url&Query=" + c.replace(/\//g, "%2F");
+    else {
+        let a = prompt("Enter a url to search for in siteimprove:\r\ne.g. %27/contact-dfe%27", "").replace(/\//g, "%2F");
+        a && (location = "https://my2.siteimprove.com/QualityAssurance/1112165/Overview/Search?SearchIn=Url&Query=" + escape(a));
+    }
+};
+
